9bc49bd06139ce3abed1e14eebeda3004618849b
Suraj Tiwari <tiwarisuraj.in@gmail.com>
2026-02-03 18:34:44 +0530

    Add files via upload

---
+++ circle_calc.py
@@ -0,0 +1,8 @@
+def circle_calc ():
+    radius = int(input("Enter the radius: "))
+    diameter = 2*radius
+    area_of_circle = 22/7*radius**2
+    print(f"Diameter of circle: {diameter}\nArea of circle is : {area_of_circle}")
+
+circle_calc()
+
