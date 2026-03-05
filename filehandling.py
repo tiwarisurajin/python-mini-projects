faf3d5ad3d6e1666550489ccfdffd9530225152f
Suraj Tiwari <tiwarisuraj.in@gmail.com>
2026-03-05 19:50:55 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,6 @@
+with open("revision.py/registration.txt","a") as file:
+    content = file.write("Ramesh THulla Bhadwa dost")
+with open("revision.py/registration.txt","r") as file:     
+    cons = file.read()
+print(cons)    
+    
